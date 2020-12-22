6c5f88087f30520bb80f349509ded11c1e109b33
Attila Fabrik <attila.fabrik@westwing.de>
2020-12-22 20:03:33 +0100

    Add Mozi+ channel.

--- actions/update_hls/12_mozi_plusz.sh
+++ actions/update_hls/12_mozi_plusz.sh
@@ -14,24 +14,9 @@ if [[ $(_isSkippedStep "${__STEP}") == "0" ]]; then
 fi
 
 # Comment line in siptv.m3u over the HLS definition.
-CHANNEL="# Mozi+"
+CHANNEL="# Mozi Plusz"
 FILENAME="moziPlusz.m3u8"
 URL=${MOZI_PLUSZ}
 
-# handle --debug option
-if [[ "${ADEBUG}" -eq 0 ]] && [[ ! -f ${LOCAL_PATH} ]]; then
-  __msg_error "File ${LOCAL_PATH} does not exists! Make sure you provide the file in --debug mode!"
-  return "${FAILURE}"
-fi
-
-# Download the channel's source file.
-curl -o ${LOCAL_DIR}/${FILENAME} ${URL}
-
-# Store the channel IPTV address.
-SOURCE_URL=$(grep -A1 '#EXTM3U' ${LOCAL_DIR}/${FILENAME} | tail -1)
-
-# Update channel address.
-if [[ $(updateChannelAddress "${CHANNEL}" "${SOURCE_URL}") == "0" ]]; then
-  __msg_info "${CHANNEL} Updated!"
-  return "${SUCCESS}"
-fi
+# shellcheck source=./helper/m3u8.sh
+source "${__DIR}"/helper/m3u8.sh
